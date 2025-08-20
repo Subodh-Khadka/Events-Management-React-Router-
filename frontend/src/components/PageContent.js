b2f565f70e4fdb10fd16dfd6dfa14285646681bc
Subodh-Khadka <subodhkdk1100@gmail.com>
2025-08-20 15:31:10 +0545

    error handling

---
+++ frontend/src/components/PageContent.js
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
+import classes from "./PageContent.module.css";
+
+function PageContent({ title, children }) {
+  return (
+    <div className={classes.content}>
+      <h1>{title}</h1>
+      {children}
+    </div>
+  );
+}
+
+export default PageContent;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -4,6 +4,9 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
   const events = data.events;
 
   return (
@@ -19,7 +22,10 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //
+    // return { isError: true, message: "could not fetch events..." };
+    throw new Response(JSON.stringify({ message: "Couldnot fetch events" }), {
+      status: 500,
+    });
   } else {
     return response;
   }
